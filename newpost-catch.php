d5984b1a5507250a6cf0a63ddba4bb6b45fd1ebb
s56bouya <s56bouya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-25 04:59:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/newpost-catch/trunk@523280 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- newpost-catch.php
+++ newpost-catch.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Newpost Catch
-Plugin URI: http://www.imamura.biz/blog/cms/wordpress/3614/
+Plugin URI: http://www.imamura.biz/blog/newpost-catch/
 Description: Thumbnails in new articles setting widget.
 Version: 1.0.1
 Author: Tetsuya Imamura
